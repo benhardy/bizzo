c3d93894a83782e6f5e8cc65ec01d17a063d1f9d
benhardy <benhardy+github@gmail.com>
2017-08-20 19:53:45 -0400

    run full sonar only on master, and on diffs for PRs
    * better branch detection for sonar
    
    * compile before running sonar-scanner on PRs
    
    * separate master and PR build techniques
    
    * do build scripting in a script

---
+++ .travis-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+set -o errexit
+
+if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST_BRANCH" = "" ]; then
+    mvn -B -fae install sonar:sonar
+else
+    mvn -B -fae clean compile
+    sonar-scanner -X -Dsonar.projectKey=net.bhardy.bizzo:bizzo-core -Dsonar.sources=src
+    mvn -B -fae install
+fi
+
