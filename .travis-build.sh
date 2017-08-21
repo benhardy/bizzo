cd760da2cc31290fff6225c315fd8ab823833b0c
benhardy <benhardy+github@gmail.com>
2017-08-20 21:54:26 -0400

    tell sonar-scanner where binaries are

--- .travis-build.sh
+++ .travis-build.sh
@@ -6,7 +6,7 @@ if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST_BRANCH" = "" ]; then
     mvn -B -fae install sonar:sonar
 else
     mvn -B -fae clean compile
-    sonar-scanner -X -Dsonar.projectKey=net.bhardy.bizzo:bizzo-core -Dsonar.sources=src
+    sonar-scanner -X -Dsonar.projectKey=net.bhardy.bizzo:bizzo-core -Dsonar.sources=src -Dsonar.java.binaries=./target/classes
     mvn -B -fae install
 fi
 
